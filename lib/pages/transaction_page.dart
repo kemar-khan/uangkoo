fdeb857b11f5b3379443f52218caa72bfcf0663e
kemar-khan <akmalfelicity@gmail.com>
2025-08-27 12:50:08 +0800

    commit
    transaction page siap

--- lib/pages/transaction_page.dart
+++ lib/pages/transaction_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class TransactionPage extends StatefulWidget {
   const TransactionPage({super.key});
@@ -10,6 +11,10 @@ class TransactionPage extends StatefulWidget {
 
 class _TransactionPageState extends State<TransactionPage> {
   bool isExpense = true;
+  List<String> list = ['Food & Groceries', 'Petrol', 'Others'];
+  late String dropDownValue = list.first;
+  TextEditingController dateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +30,7 @@ class _TransactionPageState extends State<TransactionPage> {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
@@ -67,6 +73,67 @@ class _TransactionPageState extends State<TransactionPage> {
                     ),
                   ),
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  'Category',
+                  style: GoogleFonts.montserrat(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: DropdownButton<String>(
+                  value: dropDownValue,
+                  isExpanded: true,
+                  icon: const Icon(Icons.arrow_downward),
+                  items: list.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropDownValue = newValue!;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: TextField(
+                  readOnly: true,
+                  controller: dateController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Date',
+                    border: UnderlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onTap: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2099),
+                    );
+                    if (pickedDate != null) {
+                      String formattedDate = DateFormat(
+                        'yyyy-MM-dd',
+                      ).format(pickedDate);
+
+                      dateController.text = formattedDate;
+                    }
+                  },
+                ),
+              ),
+              SizedBox(height: 15),
+              Center(
+                child: ElevatedButton(onPressed: () {}, child: Text('Save')),
               ),
             ],
           ),
